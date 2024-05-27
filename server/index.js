9904b1310187cecdc331aaf4614e0e4ae3a63e7c
Michael Agbeyebiawo <agbeyebiawomicheal@gmail.com>
2024-05-27 12:31:24 +0000

    over 10000 changes saved

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-const express = require("express") 
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
+dotenv.config()
+const app = express()
+
+mongoose.connect(process.env.MONGO).
+then(()=>console.log('db connected')).catch(err=>{
+    console.log(err)
+})
+
+app.listen(4000,()=>{
+    console.log('server started')
+})
+
+app.use('/auth',userRouter)
