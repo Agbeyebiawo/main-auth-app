4a6d29b9c38148aa4662d613b4189b51c5609c57
Michael Agbeyebiawo <agbeyebiawomicheal@gmail.com>
2024-05-27 14:02:17 +0000

    created signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcrypt'
+import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie-parser'
 
 export const signup = async(req,res,next)=>{
     try{
@@ -10,6 +13,26 @@ export const signup = async(req,res,next)=>{
         res.status(201).json({message:'User created successfully'})
     }catch(error){
         // res.status(400).json({message:error.message})
+        next(error)
+    }
+}
+
+export const signin = async(req,res,next)=>{
+    try{
+        const {email,password} = req.body
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(401,'Invaild credentials'))
+        }
+        const comparePassword = bcrypt.compareSync(password,validUser.password)
+        if(!comparePassword){
+            return next(errorHandler(401,'Wrong credentials'))
+        }
+        const token = await jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const {password:hashedPassword, ...rest} = validUser._doc
+        const expiryDate = new Date(Date.now() + 3600000)
+        res.cookie('access_token',token, {httpOnly: true, expires: expiryDate}).status(200).json(rest)
+    }catch(error){
         next(error)
     }
 }
